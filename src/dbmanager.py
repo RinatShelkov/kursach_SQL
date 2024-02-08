43a469cf6752f07c1c8a8d422b2bff68bf94fab1
rinat shelkov <rinarserov@icloud.com>
2024-02-08 21:50:24 +0500

    get_all_vacancies

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -16,9 +16,23 @@ class DBManager():
 
     def get_companies_and_vacancies_count(self):
         """Получает список всех компаний и количество вакансий у каждой компании"""
-        with self.conn.cursor() as cursor:
-            cursor.execute(
-                "SELECT employers.company_name, COUNT(vacancies.vacancy_id) FROM employers LEFT JOIN vacancies USING(employer_id) GROUP BY employers.company_name ")
-            return cursor.fetchall()
+        try:
+            with self.conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT employers.company_name, COUNT(vacancies.vacancy_id) FROM employers LEFT JOIN vacancies USING(employer_id) GROUP BY employers.company_name ")
+                return cursor.fetchall()
+        finally:
+            self.conn.close()
+    def get_all_vacancies(self):
+        """Получает список всех вакансий с указанием названия компании,
+         названия вакансии и зарплаты и ссылки на вакансию"""
+        try:
+            with self.conn.cursor() as cursor:
+                cursor.execute(
+                    "SELECT vacancy_name, vacancy_salary, salary_currency, vacancy_url, employers.company_name FROM vacancies LEFT JOIN employers USING(employer_id)")
+                return cursor.fetchall()
+        finally:
+            self.conn.close()
+
 
 
